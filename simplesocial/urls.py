5d79bc0da1f0348302ee7af8e2a0fc1a053363cf
geestdijk <oleksiy.web@gmail.com>
2020-02-18 12:30:15 +0200

    Made first migration with accounts pp done

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r"^$", views.HomePage.as_view(), name="home"),
+    path(r"^test/$", views.TestPage.as_view(), name="test"),
+    path(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
+    path(r"^accounts/", include("accounts.urls", namespace="accounts")),
+    path(r"^accounts/", include("django.contrib.auth.urls")),
 ]
