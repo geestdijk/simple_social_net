1b03996e7c60b77f1b342f5f17bcc97ae9992700
geestdijk <oleksiy.web@gmail.com>
2020-02-18 18:31:09 +0200

    Full copy done

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
-from django.db import models
 from django.contrib import auth
+from django.db import models
 from django.utils import timezone
 
+
 class User(auth.models.User, auth.models.PermissionsMixin):
+    
     def __str__(self):
-        return f'@{self.username}'
+        return "@{}".format(self.username)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = "accounts"
+app_name = 'accounts'
 
 urlpatterns = [
-    path(r"login/$", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path(r"logout/$", auth_views.LogoutView.as_view(), name="logout"),
-    path(r"signup/$", views.SignUp.as_view(), name="signup"),
+    url(r"login/$", auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+    url(r"logout/$", auth_views.LogoutView.as_view(), name="logout"),
+    url(r"signup/$", views.SignUp.as_view(), name="signup"),
 ]
